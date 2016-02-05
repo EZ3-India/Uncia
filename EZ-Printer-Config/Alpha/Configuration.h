db9ff83724efac75fc1894ef5ddef11da3d0a77d
Ashwin Prasad <iashwin.prasada@gmail.com>
2016-02-05 17:06:11 +0530

    Uncia 2.0
    - minor bug fix
    - removed excess LCD bitmaps
    - changed mini x/y limits

--- EZ-Printer-Config/Alpha/Configuration.h
+++ EZ-Printer-Config/Alpha/Configuration.h
@@ -482,7 +482,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,1600,100}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,100}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 3, 45}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,10,5000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
